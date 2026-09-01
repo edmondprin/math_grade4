e86ed4390f644b07b4d135e662c5ba0696b251bb
eprin <edmond_prin@apple.com>
2026-08-31 20:22:01 -0500

    reworked separation of concerns

--- math_ultim.py
+++ math_ultim.py
@@ -1,6 +1,7 @@
 import random
 
 
+
 def pick_topic():
     options_list = ["tens", "hundreds", "thousands", "tens of thousands", "hundreds of thousands", "millions"]
     return random.choice(options_list)
@@ -32,49 +33,43 @@ def generate_numbers(topic, count=3):
 
 def gather_answers(topic, numbers):
     responses = []
-    for number in range(len(numbers)):
+    for number in numbers:
         while True:
-            answer = input(f"What is the nearest {topic} for {numbers[number]:,}? ")
+            answer = input(f"What is the nearest {topic} for {number:,}? ")
             try:
-                answer = int(answer)
-                responses.append(answer)
+                responses.append(int(answer))
                 break
             except ValueError:
                 print("You need to enter a number!")
     return responses
 
 
-def calculate_answers(topic, numbers):
-    results = []
-    expected = list(map(lambda x: round(x, ROUNDING_PLACES[topic]), numbers))
-    for number in range(len(numbers)):
-        results.append(expected[number])
-    return results
+def calculate_expected_answers(topic, numbers):
+    rounding_place = ROUNDING_PLACES[topic]
+    return [round(number, rounding_place) for number in numbers]
 
 def check_answers(topic, numbers, responses):
-     expected_answers = calculate_answers(topic, numbers)
+     expected_answers = calculate_expected_answers(topic, numbers)
      return [
           expected == response
           for expected, response in zip(expected_answers, responses)
      ]
 
 def display_results(results):
-    points = 0
-    for a in results:
-        points += 1 if a == True else 0
-    print(f"Congrats! You earned a total of {points} points!")
+    points = sum(results)
+    print(f"Congrats! You earned a total of {points} points out of {len(results)}!")
 
 def main():
      topic = pick_topic()
-     print(topic)
-     numbers = generate_numbers(topic, count=3)
-     print(numbers)
+    #  print(topic)
+     numbers = generate_numbers(topic, count=1)
+    #  print(numbers)
      responses = gather_answers(topic, numbers)
-     print(f" Responses: {responses}")
-     test = calculate_answers(topic, numbers)
-     print(test)
+    #  print(f" Responses: {responses}")
+     test = calculate_expected_answers(topic, numbers)
+    #  print(test)
      results = check_answers(topic, numbers, responses)
-     print(results)
+    #  print(results)
      display_results(results)
 
 
